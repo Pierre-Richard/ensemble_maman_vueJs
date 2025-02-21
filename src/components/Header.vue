7db527211675159fbaa0e5d13da5c9a6307311d8
Pierre-Richard <mayer_alexandre@yahoo.fr>
2025-02-21 11:31:01 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-img src="../assets/logo.svg" />
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+  </v-app>
+  <carousel />
+</template>
+
+<script lang="ts">
+import { Component, Vue, toNative } from "vue-facing-decorator";
+@Component
+class Header extends Vue {}
+
+export default toNative(Header);
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav {
+  width: 100%;
+  height: 115px;
+}
+
+.logo {
+  width: 250px;
+  height: 113px;
+}
+
+img {
+  width: 250px;
+  height: 113px;
+  background-color: yellow;
+}
+</style>
